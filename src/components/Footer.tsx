b7055322523bb09b82adc618a376213e9146e922
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-11-11 23:31:51 +0100

    Layout updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
                     <span className="ml-3 text-xl">fsbnotes</span>
                 </a>
                 <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2023 -
-                    <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@meddhiaka</a>
+                    <a href="https://github.com/meddhiaka" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@meddhiaka</a>
                 </p>
             </div>
         </footer>
