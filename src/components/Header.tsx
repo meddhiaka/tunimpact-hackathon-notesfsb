17bfa6f9f42909f9eaf212432e8910f1b82dd1d8
Med Dhia Kassab <mohameddhia.kassab@fsb.ucar.tn>
2023-11-12 03:01:00 +0100

    Added home link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,10 +8,10 @@ export default function Header() {
             <header className="text-gray-600 body-font bg-yellow-100 header-bg">
                 <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                     <Link href="/" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                        <Image src="/notes.png" width={80} height={80} alt="" />
+                        <Image src="/notes.png" width={80} height={80} alt="" className="transform scale-150" />
                     </Link>
                     <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
-                        <a className="mr-5 hover:text-gray-900 bg-yellow-600 text-gray-100 border-0 py-1 px-3 rounded cursor-pointer">All notes</a>
+                        <Link href="/home" className="mr-5 hover:text-gray-900 bg-yellow-600 text-gray-100 border-0 py-1 px-3 rounded cursor-pointer">All notes</Link>
                         <Link href="/signup" className="mr-5 hover:text-gray-900  bg-yellow-600 text-gray-100 border-0 py-1 px-3 rounded">Register</Link>
                     </nav>
                     <Link className="inline-flex items-center bg-gray-100 text-yellow-600 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0" href="/signin">Login
